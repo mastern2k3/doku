63db1b0c0240d6ad374ac1222bb647d739b3a921
Nitzan Zada <nitzan.zada@gmail.com>
2018-06-18 10:41:19 +0300

    compiles

--- backend/src/main/scala/com/kaizoku/doku/documents/plugins/HashtagPlugin.scala
+++ backend/src/main/scala/com/kaizoku/doku/documents/plugins/HashtagPlugin.scala
@@ -1,6 +1,7 @@
 package com.kaizoku.doku.documents.plugins
 
 import scala.concurrent.Future
+import org.h2.mvstore._
 
 import com.kaizoku.doku.documents._
 
@@ -8,6 +9,8 @@ class HashtagPlugin extends TypedDocumentPlugin[MapData] {
 
   def uniqueName = "hashtag"
 
+  val jew = MVStore.open("koklol")
+
   def process(doc: DocumentDetails, body: DocumentBody, metadata: Option[MapData]): Future[Option[MapData]] =
-    Future(metadata)
+    Future.successful(metadata)
 }
